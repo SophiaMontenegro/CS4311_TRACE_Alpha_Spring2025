c3fc7cd0a6860446745bf6d0bd4a1ed38f93fcb1
Christian Rodriguez <christianrodriguez@Christians-MacBook-Air-9.local>
2025-04-23 20:06:03 -0600

    Cleaned pycache and updated tree logic

--- Backend/Team1/webtree/routes.py
+++ Backend/Team1/webtree/routes.py
@@ -8,7 +8,7 @@ router = APIRouter()
 tree_builder = WebTreeBuilder(
     uri="bolt://localhost:7687",
     user="neo4j",
-    password="chrischris21"  # <- your password here
+    password="tree-test"
 )
 controller = WebTreeController(tree_builder)
 
