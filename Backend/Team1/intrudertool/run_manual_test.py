4c9901e726bee1dab33564801728b11e9372e887
Eric Gardea <eric.gardea29513@gmail.com>
2025-04-25 12:07:30 -0600

    Added additional testing url

--- Backend/Team1/intrudertool/run_manual_test.py
+++ Backend/Team1/intrudertool/run_manual_test.py
@@ -14,6 +14,7 @@ from urllib.parse import urlparse
 #url = "https://httpbin.org/anything"
 #url = "https://api.publicapis.org/entries" #Note this is website is down which will give you "DNS resolution failed" error
 #url = "https://www.formsite.com/html5/"
+url = "https://juice-shop.herokuapp.com"
 
 # Validate URL format
 def is_valid_url(url: str) -> bool:
